4c7ab7fca67439b43308790729af2aec62a58573
Josh <36666883+Hibyehello@users.noreply.github.com>
2023-03-25 20:44:15 -0500

    Create stub for the utils module
    Update the stub's __init__.py

---
+++ python-stubs/dolphin/utils.pyi
@@ -0,0 +1,56 @@
+"""
+Module for various utilities
+"""
+
+def get_script_dir() -> str:
+	"""
+	Gets the path to the Scripts dir
+
+	:return: value as string
+	"""
+
+def open_dir() -> str:
+	"""
+	Promtps the user to open a file using a QFileDialog
+
+	:return: value as string
+	"""
+
+def start_framedump():
+	"""
+	Starts a framedump
+	"""
+
+def stop_framedump():
+	"""
+	Stops a framedump
+	"""
+
+def is_framedumping() -> bool:
+	"""
+	Gets if a framedump is occuring
+
+	:return: value as bool
+	"""
+
+def start_audiodump():
+	"""
+	Starts an audiodump
+	"""
+
+def stop_audiodump():
+	"""
+	Stops an audiodump
+	"""
+
+def is_audiodumping() -> bool:
+	"""
+	Gets if an audiodump is occuring
+
+	:return: value as bool
+	"""
+
+def save_screenshot(filename = None):
+	"""
+	Saves a screenshot of the running game
+	"""
